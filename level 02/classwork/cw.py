1b4612c07ddfb491937e0ed6146709888ac53e72
soso635 <sosokhutsishvili2203@gmail.com>
2025-05-02 00:30:08 +0400

    commit

---
+++ level 02/classwork/cw.py
@@ -0,0 +1,12 @@
+#string
+"hello pal"
+
+#integer
+83
+
+#float
+22.2
+
+#boolean
+smth = True
+anything = False
---
+++ level 02/homework/hw.py
@@ -0,0 +1,48 @@
+name = input("enter your name:")
+
+print(name)
+
+
+#maths
+#mimateba
+print(1+2)
+print(3+5)
+print(9+1)
+print(7+7)
+print(5+5)
+
+#gamokleba
+print(3-1)
+print(6-8)
+print(10-4)
+print(134-44)
+print(44-16)
+
+#gamravleba
+print(34*2)
+print(573*45)
+print(7*8)
+print(55*44)
+print(1*1)
+
+#gayofa
+print(99/11)
+print(74/7)
+print(32/5)
+print(90/5)
+print(76/76)
+
+
+#ME
+
+name = "soso"
+surname = "khutsishvili"
+age = 17
+fav_sport = "formula 1"
+fav_driver = "hamilton" 
+
+print(name)
+print(surname)
+print(age)
+print(fav_sport)
+print(fav_driver)
