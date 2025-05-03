732d28b02361afa61b05d60dd570ebbb300252e7
soso635 <sosokhutsishvili2203@gmail.com>
2025-05-04 01:11:12 +0400

    commit

---
+++ level 3/classwork/cw.py
@@ -0,0 +1,12 @@
+name = "Alice"
+age = 25
+height = 5.4
+
+print(name  +  name)
+print(age + age)
+print(height + height)
+print("hello" " " + name + " Your age is "  + str(age))
+
+
+
+
---
+++ level 3/homework/hw.py
@@ -0,0 +1,37 @@
+#1) შექმენით 3 ცვლადი სადაც შეინახავთ თქვენს სახელს, გვარს და ასაკს.
+
+name = "soso"
+surname = "khutsishvili"
+age = 17
+
+#2) ტერმინალში უნდა გამოიტანოთ ეს ცვლადები ერთ ხაზზე.
+
+print(name, surname, age)
+
+#3) ეცადეთ, რომ ლამაზად გამოიტანოთ ტერმინალში.
+
+print("hello," "my name is" " " + name  +" " "my surname is" " " + surname, "i am" " " +  str(age)  + " " "years old"  )
+
+ #შექმენით 5 ცვლადი სადაც შეინახავთ რიცხვებს. ამ ცვლადებზე უნდა მოახდინოთ ოპერაციები, მაგალითად მიმატება, გამოკლება, გამრავლება და გაყოფა. 
+
+num_1 = 44
+num_2 = 22
+num_3 = 16
+num_4 = 33
+num_5 = 15
+
+print(num_1 + num_5)
+print(num_3 + num_2)
+print(num_4 + num_4)
+
+print(num_1 / num_2)
+print(num_4 / num_3)
+print(num_3 / num_3)
+
+print(num_4 - num_1)
+print(num_3 - num_5)
+print(num_1 - num_2)
+
+print(num_2 * num_1)
+print(num_5 * num_3)
+print(num_2 * num_4)
