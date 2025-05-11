4f228bd3585d8acbe2b33ad3bb282fc114529789
soso635 <sosokhutsishvili2203@gmail.com>
2025-05-11 13:35:02 +0400

    commit

---
+++ level 05/homework/hw.py
@@ -0,0 +1,38 @@
+#1)შემოატანინეთ მომხმარებელს რიცხვი  და თუ 10 ზე მეტი იქნება  გამოიტანინოს ("you are right ")
+
+if True:
+    input("enter num:")
+
+print("you are right")
+
+
+#2)მომხმარებელს შემოატანინეთ ორი რიცხვი გააკეთეთ მათზე მათემატიკური ოპერაციები  ("+, -, *, /, %, <, >, <=, >=, ==, !=. **)
+
+
+x = input()
+y = input()
+
+print(int(x) + int(y))
+print(int(x) - int(y))
+print(int(x) * int(y))
+print(int(x) / int(y))
+print(int(x) % int(y))
+print(int(x) > int(y))
+print(int(x) < int(y))
+print(int(x) <= int(y))
+print(int(x) >= int(y))
+print(int(x) <= int(y))
+print(int(x) == int(y))
+print(int(x) != int(y))
+print(int(x) ** int(y))
+
+
+
+
+
+
+#3)ახსენით დღეს ნასწავლი If კომენტარებით
+
+#ვიყენებთ როდესაც რაიმის შემოწმება გვიწევს ვიყენებთ if ფუნქციას
+
+
