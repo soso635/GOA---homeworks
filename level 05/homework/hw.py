f857a314dee25393dc90a0f0dbef4542ab4572a2
soso635 <sosokhutsishvili2203@gmail.com>
2025-05-13 09:15:33 +0400

    commit

--- level 05/homework/hw.py
+++ level 05/homework/hw.py
@@ -1,9 +1,11 @@
 #1)შემოატანინეთ მომხმარებელს რიცხვი  და თუ 10 ზე მეტი იქნება  გამოიტანინოს ("you are right ")
 
-if True:
-    input("enter num:")
 
-print("you are right")
+number = input("enter ur num: ")
+if int(number) > 10:
+    print("you are right")
+
+
 
 
 #2)მომხმარებელს შემოატანინეთ ორი რიცხვი გააკეთეთ მათზე მათემატიკური ოპერაციები  ("+, -, *, /, %, <, >, <=, >=, ==, !=. **)
